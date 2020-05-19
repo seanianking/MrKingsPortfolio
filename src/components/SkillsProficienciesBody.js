5b83a6e47b02f58ecc38e74a36bd36d3af2fda68
seanianking <seanianking@gmail.com>
2020-05-19 13:45:05 -0600

    fixed react and adobe images for skills page

--- src/components/SkillsProficienciesBody.js
+++ src/components/SkillsProficienciesBody.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './navbar'
-import ReactLogo from '../assets/images/reactlogo.jpg'
-import Adobe from '../assets/images/adobe.jpeg'
+import ReactLogo from '../assets/images/React.png'
+import Adobe from '../assets/images/adobe.svg'
 import MySQL from '../assets/images/mysql.svg'
 import Mongo from '../assets/images/mongodb.svg'
 import Vanilla from '../assets/images/vanillaJS.svg'
@@ -28,68 +28,68 @@ function SkillsProficienciesBody() {
                         <br />
                         <div className='columns is-w-90 has-text-centered'>
                             <div className='column'>
-                            <h2 className="headerItem">Front-End</h2>
-                            <div className="card-image">
-                                        <figure className="image ">
-                                            <img src={ReactLogo} alt="Streaming Site Screenshot" />
-                                        </figure>
-                                    </div><div className="card-image">
-                                        <figure className="image ">
-                                            <img src={HTMLLogo} alt="Streaming Site Screenshot" />
-                                        </figure>
-                                    </div><div className="card-image">
-                                        <figure className="image ">
-                                            <img src={Vanilla} alt="Streaming Site Screenshot" />
-                                        </figure>
-                                    </div><div className="card-image">
-                                        <figure className="image ">
-                                            <img src={jQuery} alt="Streaming Site Screenshot" />
-                                        </figure>
-                                    </div><div className="card-image">
-                                        <figure className="image ">
-                                            <img src={CSS} alt="Streaming Site Screenshot" />
-                                        </figure>
-                                    </div><div className="card-image">
-                                        <figure className="image ">
-                                            <img src={Bulma} alt="Streaming Site Screenshot" />
-                                        </figure>
-                                    </div><div className="card-image">
-                                        <figure className="image ">
-                                            <img src={Bootstrap} alt="Streaming Site Screenshot" />
-                                        </figure>
-                                    </div>
+                                <h2 className="headerItem">Front-End</h2>
+                                <div className="card-image">
+                                    <figure className="image ">
+                                        <img src={ReactLogo} alt="Streaming Site Screenshot" />
+                                    </figure>
+                                </div><div className="card-image">
+                                    <figure className="image ">
+                                        <img src={HTMLLogo} alt="Streaming Site Screenshot" />
+                                    </figure>
+                                </div><div className="card-image">
+                                    <figure className="image ">
+                                        <img src={Vanilla} alt="Streaming Site Screenshot" />
+                                    </figure>
+                                </div><div className="card-image">
+                                    <figure className="image ">
+                                        <img src={jQuery} alt="Streaming Site Screenshot" />
+                                    </figure>
+                                </div><div className="card-image">
+                                    <figure className="image ">
+                                        <img src={CSS} alt="Streaming Site Screenshot" />
+                                    </figure>
+                                </div><div className="card-image">
+                                    <figure className="image ">
+                                        <img src={Bulma} alt="Streaming Site Screenshot" />
+                                    </figure>
+                                </div><div className="card-image">
+                                    <figure className="image ">
+                                        <img src={Bootstrap} alt="Streaming Site Screenshot" />
+                                    </figure>
+                                </div>
                             </div>
                             <div className='column'>
-                            <h2 className="headerItem">Back-End</h2>
-                            <div className="card-image">
-                                        <figure className="image ">
-                                            <img src={Mongo} alt="Streaming Site Screenshot" />
-                                        </figure>
-                                    </div><div className="card-image">
-                                        <figure className="image ">
-                                            <img src={MySQL} alt="Streaming Site Screenshot" />
-                                        </figure>
-                                    </div><div className="card-image">
-                                        <figure className="image ">
-                                            <img src={NodeJS} alt="Streaming Site Screenshot" />
-                                        </figure>
-                                    </div><div className="card-image">
-                                        <figure className="image ">
-                                            <img src={Express} alt="Streaming Site Screenshot" />
-                                        </figure>
-                                    </div>
+                                <h2 className="headerItem">Back-End</h2>
+                                <div className="card-image">
+                                    <figure className="image ">
+                                        <img src={Mongo} alt="Streaming Site Screenshot" />
+                                    </figure>
+                                </div><div className="card-image">
+                                    <figure className="image ">
+                                        <img src={MySQL} alt="Streaming Site Screenshot" />
+                                    </figure>
+                                </div><div className="card-image">
+                                    <figure className="image ">
+                                        <img src={NodeJS} alt="Streaming Site Screenshot" />
+                                    </figure>
+                                </div><div className="card-image">
+                                    <figure className="image ">
+                                        <img src={Express} alt="Streaming Site Screenshot" />
+                                    </figure>
+                                </div>
                             </div>
                             <div className='column'>
-                            <h2 className="headerItem">Other/Misc</h2>
-                            <div className="card-image">
-                                        <figure className="image ">
-                                            <img src={Adobe} alt="Creative Cloud" />
-                                        </figure>
-                            </div>
-                            <h3>Dad Jokes and Puns</h3>
-                            <h3>Storytelling</h3>
-                            <h3>Random Facts</h3>
-                            <h3>Movie Quotes</h3>
+                                <h2 className="headerItem">Other/Misc</h2>
+                                <div className="card-image">
+                                    <figure className="image ">
+                                        <img src={Adobe} alt="Creative Cloud" />
+                                    </figure>
+                                </div>
+                                <h3 className='subTitleItem'>Dad Jokes and Puns</h3>
+                                <h3 className='subTitleItem'>Storytelling</h3>
+                                <h3 className='subTitleItem'>Random Facts</h3>
+                                <h3 className='subTitleItem'>Movie Quotes</h3>
                             </div>
                         </div>
                     </div>
