131e4596a258f513f17ca0f1017187dceebc9a9b
seanianking <seanianking@gmail.com>
2020-05-25 09:40:14 -0600

    using bulma default nav to fix mobile responsiveness. added state hook and modifier for burger usage

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Resume from '../pages/Resume'
+
 
 function Message() {
     return (
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,82 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { CastMessageContext } from '../App'
 
-// handleBurgerState
+
 
 function Navbar() {
     const { handleMessageCasting } = useContext(CastMessageContext);
 
+    const [burgerState, setBurgerState] = useState(false);
+
+    const handleBurgerState = e => {
+        e.preventDefault();
+        console.log("add those toppings");
+        setBurgerState(!burgerState);
+    }
+
     return (
 
         <div>
-            
+            <nav className="navbar is-fixed-bottom is-primary " role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="https://bulma.io">
+                        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+                    </a>
+
+                    <a onClick={handleBurgerState} role="button" class={`navbar-burger burger ${burgerState ? "is-active": ""}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" class={`navbar-menu has-dropdown has-dropdown-up ${burgerState ? "is-active" : ""}`}>
+                    <div class="navbar-start">
+                        <a class="navbar-item">
+                            Home
+                        </a>
+
+                        <a class="navbar-item">
+                            Documentation
+                        </a>
+
+                        <div class="navbar-item has-dropdown has-dropdown-up is-hoverable">
+                            <a class="navbar-link">
+                                More
+                            </a>
+
+                            <div class="navbar-dropdown">
+                                <a class="navbar-item">
+                                    About
+                            </a>
+                                <a class="navbar-item">
+                                    Jobs
+                            </a>
+                                <a class="navbar-item">
+                                    Contact
+                            </a>
+                                <hr class="navbar-divider" />
+                                <a class="navbar-item">
+                                    Report an issue
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="navbar-end">
+                        <div class="navbar-item">
+                            <div class="buttons">
+                                <a class="button is-primary">
+                                    <strong>Sign up</strong>
+                                </a>
+                                <a class="button is-light">
+                                    Log in
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
 
         </div>
     )
@@ -21,22 +88,22 @@ export default Navbar
 
 
 <div class="navbar-brand">
+
+    <a href='/' className="navbar-item">Home</a>
     <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
     </a>
+
 </div>
 
-
 <div className="navbar-menu">
-    <a href='/' className="navbar-item">Home</a>
+    
     <a href='/Backstory' className="navbar-item">Backstory</a>
     <a href='/SkillsProficiencies' className="navbar-item">Skills/Proficiencies</a>
     <a href='/Adventures' className="navbar-item">Adventures</a>
     <a className="navbar-item" href='#' onClick={handleMessageCasting}>Send a Message</a>
-
-
 
 </div>
 </nav> */}
