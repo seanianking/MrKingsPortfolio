7c053eb11ea1a83fceccb5ade3756d5b9b972279
seanianking <seanianking@gmail.com>
2020-05-27 01:04:16 -0600

    dropdown displays on mobile, just incorrectly. Nearly have fixed for mobile.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -17,61 +17,63 @@ function Navbar() {
     return (
 
         <div>
-            <nav className="navbar is-fixed-bottom is-primary navMargin" role="navigation" aria-label="main navigation">
-                <div class="navbar-brand">
-                    <a class="navbar-item" href="https://bulma.io">
-                        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
-                    </a>
-
-                    <a onClick={handleBurgerState} role="button" className={`navbar-burger burger ${burgerState ? "is-active is-block": ""}`} aria-label="menu" aria-expanded="false" data-target="navMenu">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </a>
-                </div>
-
-                <div id="navMenu" class={`navbar-menu ${burgerState ? "is-active is-block" : ""}`}>
-                    <div class="navbar-start">
-                        <a class="navbar-item">
-                            Home
+            <nav className="navbar is-fixed-bottom is-primary" role="navigation" aria-label="main navigation">
+                <div className='container'>
+                    <div className="navbar-brand">
+                        <a className="navbar-item" href="https://bulma.io">
+                            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
                         </a>
 
-                        <a class="navbar-item">
-                            Documentation
-                        </a>
-
-                        <div class="navbar-item has-dropdown has-dropdown-up is-hoverable">
-                            <a class="navbar-link">
-                                More
-                            </a>
-
-                            <div class="navbar-dropdown">
-                                <a class="navbar-item">
-                                    About
-                            </a>
-                                <a class="navbar-item">
-                                    Jobs
-                            </a>
-                                <a class="navbar-item">
-                                    Contact
-                            </a>
-                                <hr class="navbar-divider" />
-                                <a class="navbar-item">
-                                    Report an issue
-                                </a>
-                            </div>
+                        <div id='navbarBurger' onClick={handleBurgerState} role="button" className={`navbar-burger burger ${burgerState ? "is-active" : ""}`} data-target="navMenu">
+                            <span></span>
+                            <span></span>
+                            <span></span>
                         </div>
                     </div>
 
-                    <div class="navbar-end">
-                        <div class="navbar-item">
-                            <div class="buttons">
-                                <a class="button is-primary">
-                                    <strong>Sign up</strong>
+                    <div id="navMenu" className={`navbar-menu ${burgerState ? "is-active" : ""}`}>
+                        <div className="navbar-start">
+                            <a className="navbar-item">
+                                Home
+                        </a>
+
+                            <a className="navbar-item">
+                                Documentation
+                        </a>
+
+                            <div className="navbar-item has-dropdown has-dropdown-up is-hoverable">
+                                <a className="navbar-link">
+                                    More
+                            </a>
+
+                                <div className="navbar-dropdown">
+                                    <a className="navbar-item">
+                                        About
+                            </a>
+                                    <a className="navbar-item">
+                                        Jobs
+                            </a>
+                                    <a className="navbar-item">
+                                        Contact
+                            </a>
+                                    <hr className="navbar-divider" />
+                                    <a className="navbar-item">
+                                        Report an issue
                                 </a>
-                                <a class="button is-light">
-                                    Log in
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="navbar-end">
+                            <div className="navbar-item">
+                                <div className="buttons">
+                                    <a className="button is-primary">
+                                        <strong>Sign up</strong>
+                                    </a>
+                                    <a className="button is-light">
+                                        Log in
                                 </a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -87,10 +89,10 @@ export default Navbar
 {/* <nav className="navbar is-fixed-bottom is-primary navMargin" role="navigation" aria-label="main navigation">
 
 
-<div class="navbar-brand">
+<div className="navbar-brand">
 
     <a href='/' className="navbar-item">Home</a>
-    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
