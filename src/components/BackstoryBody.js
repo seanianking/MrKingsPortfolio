3fa4002ebb3fac172370df680a2119ae9ffebaec
seanianking <seanianking@gmail.com>
2020-06-05 00:41:16 -0600

    updated backstory page

--- src/components/BackstoryBody.js
+++ src/components/BackstoryBody.js
@@ -30,20 +30,24 @@ function BackstoryBody() {
                         <div className='columns is-w-90'>
                             <div className='column'>
                                 <p className='aboutItem'>
-                                    My story starts in Provo, Utah where I grew up like any average person.
-                                    Throughout all of my schooling I did normal things like competing in
-                                    graphic design and t-shirt creation competitions through Skills USA,
-                                    playing sports like Football and Rugby, and only learning to read so 
-                                    I would understand the story of the video games I was playing.
+                                    My story starts in Provo, Utah where I grew up like any other relatively 
+                                    normal person. Throughout all of my schooling I did normal things like 
+                                    competing in graphic design and t-shirt creation competitions through 
+                                    Skills USA, playing sports like Football and Rugby, and only learning 
+                                    to read so I would understand the story of the video games I was playing.
+                                    Another of my favorite hobbies has to do with solving puzzles and riddles.
+                                    Whether at an escape room, or a family riddle contest, pushing the brain
+                                    to think in different ways is always a favorite pass-time. 
                                 </p>
                                 <p className='aboutItem'>
-                                    Puzzles and riddles have always been a favorite hobby of mine, I like 
-                                    to be challenged and pushed mentally, to learn and grow. I decided to
-                                    learn coding because I knew it would be a challenge and would provide
-                                    a number of challenges every day, a new puzzle to solve or riddle to 
-                                    answer. I've worked hard to learn how to tease out the answer to those
-                                    riddles, and my hard work paid off when I received a certificate for a
-                                    Web Development program with the University of Utah.
+                                    All of these things are what brought me to the world of coding. The
+                                    competing in graphic design provided the skillset and desire needed
+                                    for UI development. The sports and competitions paved the way to 
+                                    having the drive to work on my own, and the communication prowess
+                                    needed to work with a team. Coding provides a new puzzle to solve or 
+                                    riddle to answer. I've worked hard to learn how to tease out the answer 
+                                    to those riddles, and my hard work paid off when I received a certificate 
+                                    for a Web Development program with the University of Utah.           
                                 </p>
                                 <p className='aboutItem'>
                                     When I have a little free time, I'll usually spend it going on an adventure 
