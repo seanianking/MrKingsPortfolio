d1bc7247fb7b2c14b0f9149d5b288e99041072fb
Sean <seanianking@gmail.com>
2023-07-11 19:44:17 -0600

    Updates

---
+++ precache-manifest.f698a8e5d3121d52c1da3d05b6d915a6.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ab26968f220706fd17ec7ad73f1363",
+    "url": "/MrKingsPortfolio/index.html"
+  },
+  {
+    "revision": "3c8e021dd8059c3abe28",
+    "url": "/MrKingsPortfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b9928c7e10905b256811",
+    "url": "/MrKingsPortfolio/static/js/2.1f16eaef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MrKingsPortfolio/static/js/2.1f16eaef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8e021dd8059c3abe28",
+    "url": "/MrKingsPortfolio/static/js/main.8fc967d5.chunk.js"
+  },
+  {
+    "revision": "5e846462f12480ca38bc",
+    "url": "/MrKingsPortfolio/static/js/runtime-main.54fd4c7b.js"
+  },
+  {
+    "revision": "7483cdb622437e70fb0ecc95f6b042c1",
+    "url": "/MrKingsPortfolio/static/media/CodingResume.7483cdb6.pdf"
+  },
+  {
+    "revision": "45ec8ce02db11a71a978d6e39d57c099",
+    "url": "/MrKingsPortfolio/static/media/ExpressionsLanding.45ec8ce0.png"
+  },
+  {
+    "revision": "a18bbcd56b44ce110c4b8b886a937e25",
+    "url": "/MrKingsPortfolio/static/media/LandingPageImage.a18bbcd5.png"
+  },
+  {
+    "revision": "14c89a41e5154b4a2920df0115337ad5",
+    "url": "/MrKingsPortfolio/static/media/React.14c89a41.png"
+  },
+  {
+    "revision": "f87139c43eb5e3616d43c4a00f8503aa",
+    "url": "/MrKingsPortfolio/static/media/Sass.f87139c4.png"
+  },
+  {
+    "revision": "b58016c5258193f58e7b16f188a4ea73",
+    "url": "/MrKingsPortfolio/static/media/adobe.b58016c5.svg"
+  },
+  {
+    "revision": "71e42b5a8d6c0d6628ec8840cc4030e9",
+    "url": "/MrKingsPortfolio/static/media/bootstrap.71e42b5a.svg"
+  },
+  {
+    "revision": "6bf9b4da7da578b9d3447adc363d32e1",
+    "url": "/MrKingsPortfolio/static/media/bulma-logo.6bf9b4da.png"
+  },
+  {
+    "revision": "d3ad67fa720fd7aee8c322caff911082",
+    "url": "/MrKingsPortfolio/static/media/css3.d3ad67fa.svg"
+  },
+  {
+    "revision": "530516030e26a09ac38473bd5d8eff0e",
+    "url": "/MrKingsPortfolio/static/media/express.53051603.svg"
+  },
+  {
+    "revision": "24845c5ca600cfab149e84387d826465",
+    "url": "/MrKingsPortfolio/static/media/html5.24845c5c.svg"
+  },
+  {
+    "revision": "6ff17bd2dc7d786853da589cb373d553",
+    "url": "/MrKingsPortfolio/static/media/jQuery.6ff17bd2.svg"
+  },
+  {
+    "revision": "bbcd4feefeba1d3d72b8dc1de48143d8",
+    "url": "/MrKingsPortfolio/static/media/king239.bbcd4fee.JPG"
+  },
+  {
+    "revision": "07402800471de2e87f1a5444dec48c6e",
+    "url": "/MrKingsPortfolio/static/media/landing area.07402800.jpg"
+  },
+  {
+    "revision": "2aa7f7b569fd6e9176f49dd31eaa970e",
+    "url": "/MrKingsPortfolio/static/media/mongodb.2aa7f7b5.svg"
+  },
+  {
+    "revision": "dd2a5a358ea682bed45bf98bc842b3ed",
+    "url": "/MrKingsPortfolio/static/media/mysql.dd2a5a35.svg"
+  },
+  {
+    "revision": "f434bfbbffc44588572e13e1e113391a",
+    "url": "/MrKingsPortfolio/static/media/nodeJS.f434bfbb.svg"
+  },
+  {
+    "revision": "bdad1321d32a62f80711139186975c79",
+    "url": "/MrKingsPortfolio/static/media/plannerLanding.bdad1321.png"
+  },
+  {
+    "revision": "9ad4e256dafe261685ce4d7dbb7170ec",
+    "url": "/MrKingsPortfolio/static/media/searchPage.9ad4e256.jpg"
+  },
+  {
+    "revision": "cfab87fa9c021536d9c1fa1aa6025f9a",
+    "url": "/MrKingsPortfolio/static/media/someBurgersEaten.cfab87fa.png"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "/MrKingsPortfolio/static/media/vanillaJS.87711407.svg"
+  }
+]);
